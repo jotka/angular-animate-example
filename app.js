f6456a4bf564aed3d770dcbb3827c8264c93c7b3
Jarek Krochmalski <jkrochmalski@gmail.com>
2013-11-19 20:05:53 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = angular.module("animTodo", ["ngAnimate"]);
+
+app.controller("TodoCtrl", function($scope) {
+    $scope.items = [{text: "This is a demo todo.", complete: true}];
+    $scope.newTodo = "";
+
+    $scope.addTodo = function() {
+        $scope.items.push({text: $scope.newTodo, complete: false});
+        $scope.newTodo = "";
+    };
+
+    $scope.removeTodo = function(index) {
+        $scope.items.splice(index, 1);
+    };
+
+    $scope.clearAll = function() {
+        $scope.items = [];
+    };
+});
